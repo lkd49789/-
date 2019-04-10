31f2a6a4fa6ae8a1b68fe0f88e27402e5f4ee213
unknown <kaidi_liao@snh48.com>
2019-04-10 16:25:57 +0800

    up

---
+++ 我的小前端 - 副本/my/src/components/Li.vue
@@ -0,0 +1,39 @@
+<template>
+	<div id="li">
+		<div class="big">
+			<span>{{msg.name}}</span>
+			<p>{{msg.tetx}}</p>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'Li',
+		props:{
+			msg:{
+				default:{//页面未加载完成/成功显示的图片
+	  				name:'-.-',
+	  				txt:'-.-',
+	  				background:'blue'
+				},
+				type:Object
+			}
+		}
+	}
+</script>
+
+<style>
+	.big{
+		width: 67px;
+	}
+	.big span{
+		width: 40px;
+		height: 40px;
+		border-radius: 50%;
+		line-height: 40px;
+		text-align: center;
+		/* color: white;
+		background: 'msg.background'; */
+	}
+</style>
